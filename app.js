34ba6e3afdea8b25f3aa41661958024ad5755904
JadenR7 <jaden.a.r@outlook.com>
2025-09-09 22:57:38 +0000

    js run

---
+++ app.js
@@ -0,0 +1,19 @@
+const name = "Jaden";
+let age = 17
+const is_learning_js = true 
+console.log(name)
+console.log(age)
+console.log(is_learning_js)
+
+age *= 2
+console.log(age)
+ 
+age --  
+console.log(age)
+
+let money = 300000
+console.log(money)
+money /= 2
+console.log(money)
+money = money ** 2
+console.log(money)
